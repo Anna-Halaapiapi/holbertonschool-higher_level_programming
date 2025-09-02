d3eac3e5b6cebcf41a6bae027e3f9fac32d90c6b
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-09-02 19:12:16 +1000

    add 6-print_comb3.py

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+#for first digit
+for i in range(0, 10):
+
+    #for second digit
+    for j in range(0, 10):
+        if (i == 8) and (j == 9):
+            break
+        if (j != i) and (j != 0) and (i < j):
+            print("{0}{1}, ".format(i, j), end='')
+print("{}{}".format(8, 9), end='')
