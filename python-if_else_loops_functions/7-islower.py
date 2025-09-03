9b8425c3c9c7a3ab8db2c7e1e3e338d8edc6a167
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-09-03 12:15:09 +1000

    updare 7-islower.py

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 def islower(c):
-    """Checks for lowercase letter. Returns True for lowercase else returns False"""
+    """Checks for lowercase letter.
+    Returns True for lowercase else returns False"""
     result = ord(c)
 
     if (result >= 97) and (result <= 122):
