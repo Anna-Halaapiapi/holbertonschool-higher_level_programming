ab28b313f8a2089aaf2ed05d17d759274ec5dfec
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-09-03 13:51:18 +1000

    add 9-print_last_digit.py

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -8,6 +8,6 @@ def print_last_digit(number):
 
     elif number >= 0:
         last_digit = number % 10
-    
+
     print("{}".format(last_digit), end='')
-    return(last_digit)
+    return (last_digit)
