6f7ab377a36fb94edbc34bfe923faddb10caa05a
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-09-03 15:04:51 +1000

    update 12-fizzbuzz.py

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -5,7 +5,7 @@ def fizzbuzz():
     for multiples of 3 - prints Fizz.
     for multiples of 5 - prints Buzz.
     for multiples of 3 & 5 - prints Fizz Buzz."""
-    for i in range(1, 100):
+    for i in range(1, 101):
         if (i % 3 == 0) and (i % 5 == 0):
             print("FizzBuzz ", end='')
         elif (i % 3 == 0):
