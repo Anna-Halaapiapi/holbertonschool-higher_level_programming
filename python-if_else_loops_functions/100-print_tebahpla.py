01117b5be053892656eab2eb6df84b38fd8236bb
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-09-05 16:47:36 +1000

    add 100-print_tebahpla.py

---
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+x = 124
+y = 91
+i = 0
+letter = 0
+
+while (i <= 26):
+
+    if (i % 2 == 0):  # lowercase
+        x = x - 2
+        letter = x
+
+    elif (i % 2 != 0):  # uppercase
+        y = y - 2
+        letter = y
+
+    print("{}".format(chr(letter)), end='')
+    i = i + 1
