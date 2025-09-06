542b6c76e5094238cc873bf9eb917d350e204f3b
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-09-06 11:40:15 +1000

    add 100-my_calculator.py

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -5,7 +5,7 @@ import sys
 
 
 if __name__ == "__main__":
-    if (len(sys.argv) != 3):
+    if (len(sys.argv) != 4):
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
