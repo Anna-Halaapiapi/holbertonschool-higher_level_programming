79206fb8305f3e0e0a1bac9f344ccc05b6776cfa
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-09-08 13:05:53 +1000

    add 1-element_at.py

---
+++ python-data_structures/1-element_at.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def element_at(my_list, idx):
+    """retrieves an element from a list"""
+    if (idx < 0):
+        return None
+
+    elif (idx > (len(my_list) - 1)):
+        return None
+
+    else:
+        return my_list[idx]
+
+
+# my_list = [1, 2, 3, 4, 5]
+# idx = 3
+# print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
