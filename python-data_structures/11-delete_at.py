b59aabb9dfd0e3a0eab4b8d7d363d2d0ca79f024
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-09-08 17:36:25 +1000

    add 11-delete_at.py

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -2,7 +2,7 @@
 
 def delete_at(my_list=[], idx=0):
     """deletes item at specific position in list"""
-    if idx < 0 or idx > len(my_list):
+    if idx < 0 or idx >= len(my_list):
         return my_list
 
     del my_list[idx]
