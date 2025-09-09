08470b45b04b92427b568677a64c88bb355de0b3
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-09-09 12:38:35 +1000

    add 4-only_diff_elements.py

---
+++ python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def only_diff_elements(set_1, set_2):
+    """returns all elements present in only one set"""
+    result = set_1 ^ set_2
+    return result
+
+
+# set_1 = { "Python", "C", "Javascript" }
+# set_2 = { "Bash", "C", "Ruby", "Perl" }
+# od_set = only_diff_elements(set_1, set_2)
+# print(sorted(list(od_set)))
