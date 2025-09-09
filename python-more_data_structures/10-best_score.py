7eb57901e8161df85aa5af74c9fa726d09639ea3
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-09-09 15:25:38 +1000

    add 10-best_score.py

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -12,6 +12,8 @@ def best_score(a_dictionary):
         return None
 
     max_value = max(a_dictionary.values())
+    if max_value is None:
+        return None
 
     for key, value in a_dictionary.items():
         if value == max_value:
