d7da177ffcc7aef05f1095a66d160694af650fcd
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-09-09 15:23:33 +1000

    add 10-best_score.py

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    """returns a key with the biggest integer value.
+
+    Args:
+    a_dictionary: original dictionary.
+
+    Return: key for max value or None if dict is None.
+    """
+    if a_dictionary is None:
+        return None
+
+    max_value = max(a_dictionary.values())
+
+    for key, value in a_dictionary.items():
+        if value == max_value:
+            return key
+
+
+# a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
+# best_key = best_score(a_dictionary)
+# print("Best score: {}".format(best_key))
+
+# best_key = best_score(None)
+# print("Best score: {}".format(best_key))
