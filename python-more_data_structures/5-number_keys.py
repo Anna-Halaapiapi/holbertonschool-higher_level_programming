c1d1366fe843f89dbc139a878bdcb82d988fe66f
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-09-09 12:41:06 +1000

    add 5-number_keys.py

---
+++ python-more_data_structures/5-number_keys.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def number_keys(a_dictionary):
+    """ returns the number of keys in a dictionary"""
+    result = len(a_dictionary)
+    return result
+
+
+# a_dictionary = { 'language': "C", 'number': 13, 'track': "Low level" }
+# nb_keys = number_keys(a_dictionary)
+# print("Number of keys: {:d}".format(nb_keys))
