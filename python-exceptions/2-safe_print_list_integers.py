6a355a213d18a01fdc14fa44d00f4e33096ab373
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-09-10 12:26:02 +1000

    add 2-safe_print_list_integers.py

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    """prints the first x items of a list (ints only)
+    Args:
+    my_list: original list
+    x = number of elements to print
+
+    Return:
+    actual number of ints printed
+    """
+    i = 0
+    n = 0
+    while (i < x):
+        try:
+            print("{:d}".format(my_list[i]), end='')
+        except IndexError:
+            break
+        except ValueError:
+            i = i + 1
+            continue
+        except TypeError:
+            i = i + 1
+            continue
+        i = i + 1
+        n = n + 1
+
+    print()
+    return n
