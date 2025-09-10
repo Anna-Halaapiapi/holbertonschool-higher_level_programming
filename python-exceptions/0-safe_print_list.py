07f10b238e41a1e3d18928ab4c04cc5023d97f6a
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-09-10 11:51:33 +1000

    add 0-safe_print_list.py

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -24,19 +24,3 @@ def safe_print_list(my_list=[], x=0):
 
     print()  # print newline
     return n
-
-
-# my_list = [1, 2, 3, 4, 5]
-
-# # nb_print = safe_print_list(my_list, 2)
-# # print("nb_print: {:d}".format(nb_print))
-# # nb_print = safe_print_list(my_list, len(my_list))
-# # print("nb_print: {:d}".format(nb_print))
-
-# # test to access element beyond list's bounds: 
-# # nb_print = safe_print_list(my_list, len(my_list) + 2)
-# # print("nb_print: {:d}".format(nb_print))
-
-# # test to pass string to func
-# nb_print = safe_print_list(my_list, 'abc')
-# print("nb_print: {:d}".format(nb_print))
