83d836395b37ca2a2879717b48e73c943105cadc
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-09-10 12:28:26 +1000

    add 2-safe_print_list_integers.py

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -14,8 +14,8 @@ def safe_print_list_integers(my_list=[], x=0):
     while (i < x):
         try:
             print("{:d}".format(my_list[i]), end='')
-        except IndexError:
-            break
+        # except IndexError:
+        #     break
         except ValueError:
             i = i + 1
             continue
