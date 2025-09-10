697684289b6a83bd42c5711627b0ce11483cbbcd
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-09-10 13:09:56 +1000

    add 3-safe_print_division.py

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    """divides 2 integers and prints the result
+    Args:
+    a: int 1
+    b: int 2
+
+    Return:
+    result of division (success) or None (fail)
+    """
+    result = 0
+    try:
+        result = a / b
+
+    except ZeroDivisionError:
+        result = None
+
+    finally:
+        print("Inside result: {}".format(result))
+
+
+# a = 12
+# b = 2
+# result = safe_print_division(a, b)
+# print("{:d} / {:d} = {}".format(a, b, result))
+
+# a = 12
+# b = 0
+# result = safe_print_division(a, b)
+# print("{:d} / {:d} = {}".format(a, b, result))
