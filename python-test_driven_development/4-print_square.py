82c00dc2bcbc1f9519f4a8bb4347d36658b59962
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-09-11 22:14:37 +1000

    add 4-print_square.py

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -15,9 +15,6 @@ def print_square(size):
     if size < 0:
         raise ValueError("size must be >= 0")
 
-    if isinstance(size, float) and size < 0:
-        raise TypeError("size must be an integer")
-
     i = 0
     while (i < size):
         print("#" * size)
