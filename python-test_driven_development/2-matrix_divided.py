d9f6778a18294375be07cc159beafcbd7313f673
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-09-11 11:27:26 +1000

    add 2-matrix_divided.py and test file

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""
+
+
+"""
+
+
+def matrix_divided(matrix, div):
+    """divides all elements of a matrix
+    
+    Args:
+    matrix: list of lists that must contain ints or floats
+    div: int or float to divide the matrix by.
+
+    Return:
+    a new matrix
+    """
+    # check if div is an int or float
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+
+    # check if div is 0
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    # check if each element in matrix is an int or float
+    for row in matrix:
+        for element in row:
+            if not isinstance(element, (float, int)):
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+
+    # check if all row lengths are the same
+    check_row_len = set(map(len, matrix))
+    if not len(check_row_len) == 1:
+        raise TypeError("Each row of the matrix must have the same size")
+
+    new_matrix = []
+    row_length = len(matrix[0])
+ 
+    # build new matrix with computed values
+    for row in matrix:
+        new_row = [] * row_length
+        for element in row:
+            new_row.append(round(element / div, 2))
+        new_matrix.append(new_row)
+
+    return new_matrix
+
+
+# matrix = [
+#     [1, 2, 3],
+#     [4, 5],
+#     # [4, 4, 5, 6, 8]
+# ]
+# print(matrix_divided(matrix, 3))
+# # # print(matrix)
