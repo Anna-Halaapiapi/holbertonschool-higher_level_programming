bd1658b9000c7592efd69f49936cdc8c68f09bb3
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-09-12 11:48:19 +1000

    add 5-text_indentation.py

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+This module provides the text_indentation function.
+This function receives a string, and separates lines based on
+special characters as the separators.
+TypeError will be raised if the text passed to the function is not a string.
+"""
+
+
+def text_indentation(text):
+    """prints a text with 2 new lines after each of these characters:
+    .
+    ?
+    :
+    Args:
+    text: the original text in which to insert new lines
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    spec_char = [":", ".", "?"]
+    line = ''
+
+    for char in text:
+        line += char
+        if char in spec_char:
+            print(line.strip())
+            print("\n")
+            line = ''
+
+    # print any leftover line(s)
+    if len(line.strip()) > 0:
+        print(line)
+        print("\n")
