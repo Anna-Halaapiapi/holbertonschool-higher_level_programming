552ba481edf1d908ba6a2eb61e1ab5cd65a94ad3
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-09-15 14:20:51 +1000

    add 3-square.py

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -17,7 +17,7 @@ class Square:
             raise ValueError("size must be >= 0")
 
         self.__size = size
-    
+
     def area(self):
         area = self.__size * self.__size
         return area
