511ad5958d801445e47512e7600fcd9bcad8d64e
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-09-15 14:04:26 +1000

    add 1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module provides the Square class.
+"""
+
+
+class Square:
+    """
+    The Square class has a private attribute, size.
+    """
+    def __init__(self, size):
+        self.__size = size
