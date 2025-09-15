598401c5142dabb2f84034a87d685114d7d28edc
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-09-15 14:11:50 +1000

    add 2-square.py

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -8,7 +8,7 @@ class Square:
     """
     The Square class has a private attribute, size.
     TypeError is raised if size is not an integer.
-    ValueError is raised if size is less than 0. 
+    ValueError is raised if size is less than 0.
     """
     def __init__(self, size=0):
         if not isinstance(size, int):
