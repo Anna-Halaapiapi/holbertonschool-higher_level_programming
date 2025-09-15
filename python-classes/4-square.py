8c098029317333ff53ff5cd8d1d94e47990d2ff0
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-09-15 14:41:34 +1000

    add 4-square.py

---
+++ python-classes/4-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+This module provides the Square class.
+"""
+
+
+class Square:
+    """
+    The Square class has a private attribute, size.
+    TypeError is raised if size is not an integer.
+    ValueError is raised if size is less than 0.
+    """
+    def __init__(self, size=0):
+        # if not isinstance(size, int):
+        #     raise TypeError("size must be an integer")
+        # if size < 0:
+        #     raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    # getter
+    @property
+    def size(self):
+        return self.__size
+
+    # setter
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        area = self.__size * self.__size
+        return area
