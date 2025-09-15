600336252a6fdd2c55c7b07591aa06485667a7d8
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-09-15 14:11:06 +1000

    add 2-square.py

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -6,7 +6,9 @@ This module provides the Square class.
 
 class Square:
     """
-
+    The Square class has a private attribute, size.
+    TypeError is raised if size is not an integer.
+    ValueError is raised if size is less than 0. 
     """
     def __init__(self, size=0):
         if not isinstance(size, int):
