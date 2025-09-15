7e12f538db672500a107866a266bed207c767d9c
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-09-15 14:20:37 +1000

    add 3-square.py

---
+++ python-classes/3-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+This module provides the Square class.
+"""
+
+
+class Square:
+    """
+    The Square class has a private attribute, size.
+    TypeError is raised if size is not an integer.
+    ValueError is raised if size is less than 0.
+    """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+    
+    def area(self):
+        area = self.__size * self.__size
+        return area
