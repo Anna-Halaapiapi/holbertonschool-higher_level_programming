db5d5f60f66e72830991cded2b011e4fccbf6170
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-09-16 16:35:39 +1000

    add 6-rectangle.py

---
+++ python-more_classes/6-rectangle.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3
+"""
+This module provides the Rectangle class.
+"""
+
+
+class Rectangle:
+    """
+    This class defines a Rectangle.
+    """
+    number_of_instances = 0
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+        Rectangle.number_of_instances += 1
+
+    # width - getter
+    @property
+    def width(self):
+        return self.__width
+
+    # height - getter
+    @property
+    def height(self):
+        return self.__height
+
+    # width - setter
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    # height - setter
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.height * self.width
+
+    def perimeter(self):
+        if self.width == 0 or self.height == 0:
+            return 0
+        return ((self.width * 2) + (self.height * 2))
+
+    def __str__(self):
+        hashes = ""
+        if self.width == 0 or self.height == 0:
+            return hashes
+        row = "#" * self.width
+        length = self.height - 1
+        for i in range(0, length):
+            hashes += row
+            hashes += "\n"
+        hashes += row
+        return hashes
+
+    def __repr__(self):
+        return f"Rectangle({self.width}, {self.height})"
+
+    def __del__(self):
+        print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
