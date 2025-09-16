af19f4042a7a90916fc8e0915041b78619799931
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-09-16 14:17:36 +1000

    add 1-rectangle.py

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+This module provides the Rectangle class.
+"""
+
+
+class Rectangle:
+    """
+    This class defines a Rectangle.
+    """
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    # width - getter
+    @property
+    def width(self):
+        return self.__width
+
+    # width - setter
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    # height - setter
+    def height(self):
+        return self.__height
+
+    # height - getter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
