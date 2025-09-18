673aedcfd8db696ec3fd550d9095d4890c2bb062
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-09-18 18:01:58 +1000

    add 100-singly_linked_list.py

---
+++ python-classes/100-singly_linked_list.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python3
+
+class Node:
+    """
+    The Node class defines a node of a singly linked list.
+    """
+    def __init__(self, data, next_node=None):
+        self.data = data
+        self.next_node = next_node
+
+    # data - getter
+    @property
+    def data(self):
+        return self.__data
+
+    # data - setter
+    @data.setter
+    def data(self, value):
+        if not isinstance(value, int):
+            raise TypeError("data must be an integer")
+        self.__data = value
+
+    # next_node - getter
+    @property
+    def next_node(self):
+        return self.__next_node
+
+    # next_node - setter
+    @next_node.setter
+    def next_node(self, value):
+        if value is not None and not isinstance(value, Node):
+            raise TypeError("next_node must be a Node object")
+        self.__next_node = value
+
+
+class SinglyLinkedList:
+    """
+    The SinglyLinkedList class defines a singly linked list.
+    """
+    def __init__(self):
+        self.__head = None
+
+    def sorted_insert(self, value):
+        """
+        creates a new node & assigns value to data var.
+        inserts the new Node into the correct position in the list
+        (list is in ascending order)
+        """
+        # create a new Node and assign value to data attribute
+        new_node = Node(value, None)
+
+        # insert at beginning if list is empty
+        if self.__head is None:
+            self.__head = new_node
+            return
+
+        # find start of list (first node)
+        current_node = self.__head
+
+        # loop through list until:
+        # hitting end of list (tail/None)
+        # break loop if insertion position found
+
+        while (current_node.next_node is not None):
+
+            # check for correct position
+            if (new_node.data < current_node.next_node.data):
+                # insert node in correct position
+                new_node.next_node = current_node.next_node
+                current_node.next_node = new_node
+                return
+
+            # go to next node to check for correct position
+            current_node = current_node.next_node
+
+        # if correct position not found in loop - add new node to end of list
+        if current_node.next_node is None:
+            current_node.next_node = new_node
+            new_node.next_node = None
+            return
