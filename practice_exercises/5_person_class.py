9943e7fae4c2ca899e112f55e609779968387a41
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-09-20 16:10:29 +1000

    update 5_person_class.py

--- practice_exercises/5_person_class.py
+++ practice_exercises/5_person_class.py
@@ -52,14 +52,44 @@ class Person:
     def __str__(self):
         return f"{self.name}, {self.age} years old"
 
+class Student(Person):
+    """
+    This class inherits from Person class, and defines a Student.
+    """
+    def __init__(self, name, age, student_id):
+        super().__init__(name, age)
+        self.student_id = student_id
+
+    # student id - getter
+    @property
+    def student_id(self):
+        return self.__student_id
+    
+    # student id - setter
+    @student_id.setter
+    def student_id(self, value):
+        if not isinstance (value, str):
+            raise TypeError("Student ID must be a string")
+        self.__student_id = value
+
+    def study(self):
+        print(f"Student {self.name} is studying")
+
+    def __str__(self):
+        return f"{super().__str__()} - ID: {self.student_id}"
+
+
 p1 = Person("Bambi", 2)
 p2 = Person("Billy", 5)
 
-p1.say_hello()
-p2.say_hello()
-Person.get_population()
-print(repr(p1))
-print(repr(p2))
+# p1.say_hello()
+# p2.say_hello()
+# Person.get_population()
+# print(repr(p1))
+# print(repr(p2))
 
-print(p1)
-print(p2)
+# print(p1)
+# print(p2)
+
+s1 = Student(p1.name, p1.age, "5501")
+print(s1)
