a46066e800e264d38c59daaa2c8005f1794369b1
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-09-23 17:56:31 +1000

    add 10-square.py

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -12,9 +12,9 @@ class Square(Rectangle):
     This class defines a Square and inherits from Rectangle.
     """
     def __init__(self, size):
-        super().__init__(size, size)
-        # size validated in Rectangle -> Base Geometry
+        super().integer_validator("size", size)
         self.__size = size
+        super().__init__(size, size)
 
     def area(self):
         return super().area()
