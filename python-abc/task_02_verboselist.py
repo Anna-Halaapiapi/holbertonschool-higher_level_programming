3a6856af4d7729d96db09f12a1f69dd1244e9bcb
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-09-24 16:11:26 +1000

    update task_02_verboselist.py

--- python-abc/task_02_verboselist.py
+++ python-abc/task_02_verboselist.py
@@ -23,6 +23,8 @@ class VerboseList(list):
         uses built-in list's extend method.
         prints message after extending list with iterable.
         """
+        if not hasattr(iterable, '__iter__'):
+            raise TypeError(f"Error: {iterable} is not an iterable")
         super().extend(iterable)
         print(f"Extended the list with [{len(iterable)}] items.")
 
@@ -31,13 +33,35 @@ class VerboseList(list):
         uses built-in list's remove method.
         prints message before removing item from list.
         """
-        print(f"Removed [{item}] from the list")
-        super().remove(item)
+        try:
+            print(f"Removed [{item}] from the list")
+            super().remove(item)
+        except ValueError:
+            print(f"Error: {item} not found in list")
 
     def pop(self, index=-1):
         """
         uses built-in list's pop method.
         prints message before removing item from list.
         """
-        print(f"Popped [{self[index]}] from the list")
+        self.index_validator(index)
+        popped_item_value = self[index]
+        print(f"Popped [{popped_item_value}] from the list")
         super().pop(index)
+        return popped_item_value
+
+    def index_validator(self, index):
+        """
+        validates:
+        if index is an int
+        if list is empty
+        if index passed is valid
+        """
+        if not isinstance(index, int):
+            raise TypeError("Error: index must be an integer")
+        if len(self) == 0:
+            raise ValueError("Error: list is empty")
+        # index provided is greater available positive & negative indices
+        if index > (len(self) - 1) or index < (-len(self)):
+            raise ValueError("Error: index provided is out of range")
+        return
