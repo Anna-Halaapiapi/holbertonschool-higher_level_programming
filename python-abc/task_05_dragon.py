1daceb1ab3e4957c537f2f10a620ae9d7d65f4f7
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-09-24 17:48:17 +1000

    add task_05_dragon.py

---
+++ python-abc/task_05_dragon.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""
+This module provides the SwimMixin, FlyMixin, Dragon classes.
+"""
+
+
+class SwimMixin:
+    """
+    defines SwimMixin
+    """
+    
+    def swim(self):
+        print("The creature swims!")
+
+
+class FlyMixin:
+    """
+    defines FlyMixin
+    """
+
+    def fly(self):
+        print("The creature flies!")
+    
+
+class Dragon(SwimMixin, FlyMixin):
+    """
+    defines Dragon
+    """
+
+    def roar(self):
+        print("The dragon makes a low growl...")
+
+    def appearance(self):
+        print("The dragon has purple scales and yellow horns.")
+      
+    def abilities(self):
+        print("The dragon breathes fire!")
+
+    def name(self):
+        print("The dragon's name is Spyro.")
