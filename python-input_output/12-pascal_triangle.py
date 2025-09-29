df1b584cc03e8d1da97f0e63745b507c24f74ab1
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-09-29 18:33:39 +1000

    add 12-pascal_triangle.py

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -6,19 +6,21 @@ This module provides the pascal_triangle function.
 
 def pascal_triangle(n):
     """
-    returns a list of lists of integers representing the Pascal's triangle of n.
+    returns a list of lists of integers representing
+    the Pascal's triangle of n.
     Args:
-    n = row length
+    n = number of rows.
     """
     if n <= 0:
         return []
 
+    # initialise triangle with first row
     p_tri = [[1]]
     i = 1
-    # outer loop to build rows
+    # outer loop to build rows from 2nd row to nth row
     while i < n:
-        
-        # start with new row of length i
+
+        # create new row of required length
         new_row = [0] * (i + 1)
 
         # access prev row for inner loop calcs
@@ -36,13 +38,13 @@ def pascal_triangle(n):
             # handle 1 at end of list
             elif j == len(new_row) - 1:
                 new_row[j] = 1
-            
+
             else:
                 # handle middle of list (not start or end)
                 new_row[j] = prev_row[j - 1] + prev_row[j]
-
             j += 1
-        # add new row to triangle
+
+        # add the new row to triangle
         p_tri.append(new_row)
 
         i += 1
