2312962a21ceb91d216eaa18c8894cc87ab0a41c
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-09-29 14:58:20 +1000

    add 10-student.py

---
+++ python-input_output/10-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+This module provides the Student class.
+"""
+
+
+class Student:
+    """
+    This class defines a Student.
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        retrieves a dictionary representation of a Student instance.
+        """
+        # return all attributes
+        if attrs is None or len(attrs) == 0:
+            return self.__dict__
+
+        # else return only attributes in list of strings
+        else:
+            new_dict = {}
+            for a in attrs:
+                if a in self.__dict__:
+                    new_dict[a] = self.__dict__[a]
+            return new_dict
