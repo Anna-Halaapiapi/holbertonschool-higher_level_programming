09311a8091e416238753fba4dd048a5bc4ea604a
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-09-29 11:48:32 +1000

    add 4-from_json_string.py

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This module provides the from_json_string function.
+"""
+
+import json
+
+
+def from_json_string(my_str):
+    """
+    returns an object (Python data structure) represented by a JSON string
+    """
+    return json.loads(my_str)
