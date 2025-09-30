fe5ea17ec9436a2cd193168f5dc55f4f70a0389c
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-09-30 16:20:09 +1000

    add task_01_pickle.py

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -25,11 +25,9 @@ class CustomObject:
         """
         prints object's attributes in format required by task.
         """
-        print(
-            f"Name: {self.name}\n"
-            f"Age: {self.age}\n"
-            f"Is Student: {self.is_student}"
-            )
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Is Student: {self.is_student}")
 
     def serialize(self, filename):
         """
