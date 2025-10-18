870f588699faefb8b0166948f92ca3f27c3172fa
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-10-18 22:09:22 +1100

    update structure of users

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -18,8 +18,8 @@ jwt = JWTManager(app)
 
 # Create a dict of users and their hashed passwords
 users = {
-    "user1": {"username": "user1", "password": generate_password_hash("password1"), "role": "user"},
-    "admin": {"username": "admin", "password": generate_password_hash("password2"), "role": "admin"}
+    "user1": {"username": "user1", "password": generate_password_hash("password"), "role": "user"},
+    "admin1": {"username": "admin1", "password": generate_password_hash("password"), "role": "admin"}
 }
 
 # create auth object to handle basic auth for routes
