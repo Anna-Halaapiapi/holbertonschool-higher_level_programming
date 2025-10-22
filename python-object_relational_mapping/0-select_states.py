9b063f99c14e550f06e9ded96cf0cdec80827109
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-10-22 20:26:33 +1100

    add 0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """
 a script that lists all states from the database hbtn_0e_0_usa
 """
@@ -7,7 +7,13 @@ import MySQLdb
 import sys
 
 # establish connection to the db
-conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+conn = MySQLdb.connect(
+    host="localhost",
+    port=3306,
+    user=sys.argv[1],
+    passwd=sys.argv[2],
+    db=sys.argv[3]
+)
 
 # create a cursor obj
 cur = conn.cursor()
