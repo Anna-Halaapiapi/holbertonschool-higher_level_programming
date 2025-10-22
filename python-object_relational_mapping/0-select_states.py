53da170c2c69292c9c31b5f20da5eef7ec963932
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-10-22 20:23:25 +1100

    add t0

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+a script that lists all states from the database hbtn_0e_0_usa
+"""
+
+import MySQLdb
+import sys
+
+# establish connection to the db
+conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+
+# create a cursor obj
+cur = conn.cursor()
+
+# get all rows of states table in ASC order
+cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+
+# fetch all results and print each row
+result = cur.fetchall()
+for row in result:
+    print(row)
