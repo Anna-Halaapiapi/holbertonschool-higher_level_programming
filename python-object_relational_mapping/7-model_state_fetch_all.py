efb8d67b6968764a9fe073dfbba6146431190da6
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-10-23 21:04:57 +1100

    add 7-model_state_fetch_all.py

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+lists all State objects from the database hbtn_0e_6_usa
+"""
+
+from sqlalchemy import Column, Integer, String, create_engine
+# from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import sys
+from model_state import Base, State
+
+if __name__ == "__main__":
+    # create connection string
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
+    port = 3306
+    host = "localhost"
+    conn_str = (
+        f"mysql+mysqldb://{username}:{password}@"
+        f"{host}:{port}/{database_name}"
+    )
+
+    # create connection between script and MySQL server
+    engine = create_engine(conn_str)
+
+    # Create a session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    ordered_list = session.query(State).order_by(State.id).all()
+
+    for item in ordered_list:
+        print(f"{item.id}: ", end='')
+        print(item.name)
