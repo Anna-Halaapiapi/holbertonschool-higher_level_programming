d4cc63b0758b87374c1a617cab69d5ed8331efdd
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-10-23 21:28:48 +1100

    add 8-model_state_fetch_first.py

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+lists all State objects from the database hbtn_0e_6_usa
+"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+from model_state import Base, State
+
+if __name__ == "__main__":
+    # create connection string
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
+    port = 3306
+    host = "localhost"
+    conn_str = (
+        f"mysql+mysqldb://{username}:{password}@"
+        f"{host}:{port}/{database_name}"
+    )
+
+    # create connection between script and MySQL server
+    engine = create_engine(conn_str)
+
+    # Create a session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    first_state = session.query(State).order_by(State.id).first()
+    if first_state is None:
+        print("Nothing")
+    else:
+        print(f"{first_state.id}: {first_state.name}")
