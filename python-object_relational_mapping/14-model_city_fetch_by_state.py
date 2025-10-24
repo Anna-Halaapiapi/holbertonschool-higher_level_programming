30e09bd4dcbb4286449cf43810c184c7f047b9c3
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-10-24 21:00:48 +1100

    add 14-model_city_fetch_by_state.py

---
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+prints all City objects from
+the database hbtn_0e_14_usa
+"""
+
+from sqlalchemy import create_engine, select
+from sqlalchemy.orm import Session
+import sys
+from model_state import Base, State
+from model_city import City
+
+if __name__ == "__main__":
+    # create connection string
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
+    port = 3306
+    host = "localhost"
+    conn_str = (
+        f"mysql+mysqldb://{username}:{password}@"
+        f"{host}:{port}/{database_name}"
+    )
+
+    # create connection between script and MySQL server
+    engine = create_engine(conn_str)
+
+    # handles open & close session
+    with Session(engine) as session:
+        # build query stmt
+        stmt = select(State.name, City.id, City.name).join(City, City.state_id == State.id).order_by(City.id)
+        # exectute query & get results
+        results = session.execute(stmt).all()
+        # print results
+        for state_name, city_id, city_name in results:
+            print(f"{state_name}: ({city_id}) {city_name}")
