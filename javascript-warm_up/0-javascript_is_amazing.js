049ab5000b55dd1584ae059d30ca10b414efa16d
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-10-29 13:32:38 +1100

    add 0-javascript_is_amazing.js

---
+++ javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// This script prints "JavaScript is amazing" to stdout.
+
+// create const var
+const myVar = 'JavaScript is amazing';
+
+// print myVar to stdout
+console.log(myVar);
