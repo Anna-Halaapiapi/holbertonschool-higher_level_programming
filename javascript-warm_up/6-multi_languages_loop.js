947363c853dd401ca9c5ca1144ffb6ebe5d1eae9
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-10-29 18:05:44 +1100

    add 6-multi_languages_loop.js

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// prints 3 lines using an array of string and a loop
+
+// define array of 3 lines to print
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+// loop through array, print each line
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
