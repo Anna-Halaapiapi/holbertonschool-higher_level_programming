ebbd3e612814799e856aae957892cc13b2d4eccf
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-10-29 20:06:25 +1100

    add 13-add.js

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -5,7 +5,4 @@ function add (a, b) {
     return a + b;
 }
 // make add visible from outside
-module.exports = { add }
-
-const add = require('./13-add').add; // import add function to test works
-console.log(add(3, 5)); // print 8 to stdout
+module.exports = add;
