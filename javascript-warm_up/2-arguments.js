cff64f9e485e8c84d8e6ccfa099a42e827490b9d
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-10-29 14:19:50 +1100

    add 2-arguments.js

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,15 +1,10 @@
 #!/usr/bin/node
 // prints a message depending of the number of arguments passed
 
-// no args passed
-if (process.argv.length === 2) {
-    console.log('No argument');
-}
-// one arg passed
-else if (process.argv.length === 3) {
-    console.log('Argument found');
-}
-// more than one arg passed
-else {
-    console.log('Arguments found');
+if (process.argv.length === 2) { // no args passed
+  console.log('No argument');
+} else if (process.argv.length === 3) { // one arg passed
+  console.log('Argument found');
+} else { // more than one arg passed
+  console.log('Arguments found');
 }
