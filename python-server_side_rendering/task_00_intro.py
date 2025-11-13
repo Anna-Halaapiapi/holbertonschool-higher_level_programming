9f2049e25d6f814460a205590871d1f141f8a20f
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-11-13 18:34:36 +1100

    t0

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 import os
 
 """
@@ -7,11 +9,12 @@ This module provides the generate_invitations function.
 
 def generate_invitations(template, attendees):
     """
-    populates template with attendees data, saves to new files.
+    populates template with attendees' data,
+    saves the updated template to individual txt files.
     """
     # check input types
     if not isinstance(template, str):
-        print("Error: template is not a string.")
+        print("Error: templateis not a string.")
         return
 
     if not isinstance(attendees, list):
@@ -31,28 +34,21 @@ def generate_invitations(template, attendees):
         print("No data provided, no output files generated.")
         return
 
-    # process each attendee
-    updated_templates = []
-    for attendee in attendees:
-        new_item = template
-        for key in attendee:
-            if attendee[key] is None:
+    # populate templates with attendee details, write to files
+    for index, attendee in enumerate(attendees, start=1):
+        updated_template = template
+        for p in ['name', 'event_title', 'event_date', 'event_location']:
+            value = attendee.get(p, "N/A")
+            if value is None:
                 value = "N/A"
-            else:
-                value = str(attendee[key])
-            new_item = new_item.replace("{" + key + "}", value)
-        updated_templates.append(new_item)
-
-    # generate output files
-    for index, item in enumerate(updated_templates, start=1):
+            updated_template = updated_template.replace("{" + p + "}", value)
         file_name = 'output_' + str(index) + '.txt'
         if os.path.exists(file_name):
-            print(f"File {file_name} already exists, unable to generate.")
+            print(f"Error: File {file_name} already exists.")
             continue
         try:
-
             with open(file_name, 'w', encoding='utf-8') as f:
-                f.write(item)
+                f.write(updated_template)
         except Exception:
-            print(f"Unable to generate {file_name}")
+            print(f"Error: Unable to generate {file_name}")
             continue
