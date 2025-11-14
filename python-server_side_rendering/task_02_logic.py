f93f63bf64e9e5abbccb99389498d6a2f38808e9
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-11-14 15:27:20 +1100

    task2

---
+++ python-server_side_rendering/task_02_logic.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+A basic flask app.
+"""
+
+from flask import Flask, render_template
+import json
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+    
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/items')
+def items():
+    # load json data
+    with open('items.json', 'r') as f:
+        data = json.load(f)
+    # pass data to items.html to use
+    return render_template('items.html', data=data)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
